c09f69a04256a08c01896e049814b1b11ea2ff20
NoamYakarOfficial <75957506+NoamYakarOfficial@users.noreply.github.com>
2021-03-25 10:06:11 -0400

    Update RL_Car.py

--- RL_Car.py
+++ RL_Car.py
@@ -1,14 +1,16 @@
 import pygame
 from kivy.vector import Vector
 from math import *
-
+#initialize pygame library before using it to manipulate anything in the program
+pygame.init()
 vector = pygame.math.Vector2
 keyboard = pygame.key.get_pressed()
-
+pygame.init()
 class RL_Car():
-    def __init__(self, x, y, angle = 0, length = 20):
+    def __init__(self, x, y, angle = 0, length = 20, max_acceleration = 5):
         self.angle = angle
         self.length = length
+        self.velocity = vector(0.0,0.0)
         #acceleration is a vector that is = to the change in velocity (derivatives :))
         self.acceleration = 0.0
         self.position = vector(x,y)
@@ -17,6 +19,9 @@ class RL_Car():
         self.steering_value = 0.0
         self.clock = pygame.time.Clock()
         self.max_steering_angle = 30
+        self.max_acceleration = max_acceleration
+        self.brake_val = 10.0
+        self.natural_deceleration = 3.3
 
     def update_position(self):
         dt = self.clock.get_time() / 1000
@@ -29,15 +34,7 @@ class RL_Car():
             angular_speed = 0.0
         self.position = self.velocity.rotate(-self.angle) * dt
         self.angle += degrees(angular_speed) * dt
-        
-        if keyboard[pygame.K_LEFT]:
-            self.acceleration.x = -0.5
-        if keyboard[pygame.K_RIGHT]:
-            self.acceleration.x = 0.5
-        if keyboard[pygame.K_UP]:
-            self.acceleration.y = 0.5
-        if keyboard[pygame.K_DOWN]:
-            self.acceleration.y = -0.5
+
 
 
 
