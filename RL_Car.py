78f9b182c181cd2d28f33d3677be74bd484dac51
NoamYakarOfficial <75957506+NoamYakarOfficial@users.noreply.github.com>
2021-03-24 21:00:02 -0400

    Update RL_Car.py

--- RL_Car.py
+++ RL_Car.py
@@ -1,25 +1,26 @@
 import pygame
 vector = pygame.math.Vector2
-events = pygame.event.get()
+keyboard = pygame.key.get_pressed()
 class RL_Car():
-    def __init__(self, x = 0, y = 0):
-        self.x = x
-        self.y = y
+    def __init__(self, x, y, angle = 0, length = 20):
         self.velocity = vector(0,0)
-        self.angle = 0
-        self.acceleration = vector(0,0)
+        self.angle = angle
+        self.length = length
+        #acceleration is a vector that is = to the change in velocity (derivatives :))
+        self.acceleration = 0.0
+        self.position = vector(x,y)
     def update_pos(self):
-        self.acceleration = vector(0,0)
-        for event in events:
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_LEFT:
-                    self.acceleration.x = -0.5
-                if event.key == pygame.K_RIGHT:
-                    self.acceleration.x = 0.5
-                if event.key == pygame.K_UP:
-                    self.acceleration.y = 0.5
-                if event.key == pygame.K_DOWN:
-                    self.acceleration.y = -0.5
+        self.velocity += (self.acceleration)
+            if keyboard[pygame.K_LEFT]:
+                self.acceleration.x = -0.5
+            if keyboard[pygame.K_RIGHT]:
+                self.acceleration.x = 0.5
+            if keyboard[pygame.K_UP]:
+                self.acceleration.y = 0.5
+            if keyboard[pygame.K_DOWN]:
+                self.acceleration.y = -0.5
+    def rotate(self):
+
 
 
 
